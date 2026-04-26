ca654fac302756fdc3b02fb6b753d71116c2f75b
Valeria TN <vticonan@unsa.edu.pe>
2026-04-26 17:42:48 -0500

    JS mostrar

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-d
+const input = document.getElementById("texto");
+
+const btnMostrar = document.getElementById("mostrar");
+
+const resultado = document.getElementById("resultado");
+
+
+btnMostrar.addEventListener("click",() =>{
+    resultado.textContent = "Tareas: " + JSON.stringify(tareas);
+});
