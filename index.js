77962d93ffb68f7ceb2564ee5ac4cd95db1062ec
Brigitte Karolay Velasquez Puma <bvelasquezp@unsa.edu.pe>
2026-04-26 20:20:40 -0500

    Boton guardar.js

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const input = document.getElementById("texto");
+const btnGuardar = document.getElementById("guardar");
+
+
 
 const btnMostrar = document.getElementById("mostrar");
 
 const resultado = document.getElementById("resultado");
 
+let tareas = [];
+btnGuardar.addEventListener("click",() =>{
+    const valor = input.value;
+    if (valor !== "") {
+        tareas.push(valor);
+        input.value = "";
+        console.log("Tarea agregada:", valor);
+    }
+});
 
 btnMostrar.addEventListener("click",() =>{
     resultado.textContent = "Tareas: " + JSON.stringify(tareas);
